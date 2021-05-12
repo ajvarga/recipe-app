dade6a28bdc906e000b2e4293cfcece2ed21e7bc
Alexander J Vargas <56698261+ajvarga@users.noreply.github.com>
2021-05-12 16:57:54 -0700

    Foundation
    I got some ideas from a tutorial and I aplan on adding upon this later. Right now we have API calls to edamam to get dishes and a working search bar and await functions.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
 function App() {
+
+  const APP_ID = 'ec098aa3'
+  const APP_KEY = '5fa3149c3e986b56d1048c3750e88bcb'
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
+
+  useEffect( () =>{
+    getRecipes()
+  }, [query]);
+
+  const getRecipes = async() => {
+    const res = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await res.json();
+
+    setRecipes(data.hits);
+    console.log(data.hits)
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value)
+  }
+
+  const getSearch = e =>{
+    e.preventDefault();
+    setQuery(search)
+    setSearch('')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className='search-form'>
+        <input className='search-bar' type='text' value={search} onChange={updateSearch}></input>
+        <button className='search-button' type='submit'>SEARCH</button>
+      </form>
+      {recipes.map(recipe =>(
+        <Recipe 
+          title={recipe.recipe.label} 
+          region={recipe.recipe.cuisineType} 
+          calories={recipe.recipe.calories} 
+          image={recipe.recipe.image}
+          ingredients={recipe.recipe.ingredients} //array
+          />
+      ))}
     </div>
   );
 }
