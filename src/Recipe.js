6b91c51bf169618610d0e4238717aac87257ca09
Alexander J Vargas <56698261+ajvarga@users.noreply.github.com>
2021-05-15 17:17:46 -0700

    messing around with css formatting
    Im trying to implement a card system but cant decide yet on how to format it atm

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import style from './recipe.module.css'
 
+/* props
+    title: string, recipe's title
+    ingredients: an array of items
+    calories: string, amount of calories in a dish
+    image: image url for food image*/
+
 const Recipe = (props) => {
     return ( 
     <div className={style.recipe}>
-        <h1>{props.title}</h1>
-        <ol>
-            {props.ingredients.map(ingredient => (
-                <li>{ingredient.text}</li>
-            ))}
-        </ol>
-        <p>calories:  {props.calories}</p>
-        <img src={props.image} alt=''/>
-    </div> 
+        <div>
+            <div className='cardFace'>
+                <h1>{props.title}</h1>
+                <img src={props.image} alt=''/>
+            </div>
+            <div className='cardBack'>
+                <ul>
+                    {props.ingredients.map(ingredient => (
+                        <li>{ingredient.text}</li>
+                    ))}
+                </ul>
+                <p>calories:  {props.calories}</p>
+                <button>Bookmark</button>
+            </div>
+        </div>
+    </div>
     );
 }
  
